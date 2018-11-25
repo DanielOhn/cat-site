be4f657154fc022e65594a169628216fb9bf3356
DanielOhn <ohndaniel@gmail.com>
2018-11-24 22:21:01 -0700

    Remove logo.  Add dogFetch and button to call the function.

--- src/App.js
+++ src/App.js
@@ -22,6 +22,27 @@ class App extends Component {
     }
   }
 
+  // CORS Issue
+  // fetchFox = async () => {
+  //   try {
+  //     const result = await fetch("https://randomfox.ca/floof");
+  //     const img = await result.json();
+  //     this.setState({ source: img.image, loading: false });
+  //   } catch (e) {
+  //     this.setState({ error: e.message, loading: false });
+  //   }
+  // }
+
+  fetchDog = async () => {
+    try {
+      const result = await fetch("https://dog.ceo/api/breeds/image/random");
+      const image = await result.json();
+      this.setState({ source: image.message, loading: false });
+    } catch (e) {
+      this.setState({ error: e.message, loading: false});
+    }
+  }
+
   render() {
     const { error, loading, source } = this.state;
     return loading ? (
@@ -35,17 +56,41 @@ class App extends Component {
             <div className="col-1"></div>
             <div className="col-10">
               <h1 className="display-1">Random Cats</h1>
-              <img src={source} className="img-fluid" alt="just a normal cat" />
+              <img src={source} className="img-fluid" alt="just a normal cat or dog" />
             </div>
             <div className="col-1"></div>
           </div>
-          <button type="button" className="btn btn-light" onClick={this.fetchCat}>
-            Get Cat
-          </button>
+          <div className="row">
+            <div className="col-4"></div>
+            <div className="col-2">
+              <Button 
+                text="Get Cat"
+                type="button"
+                className="btn btn-light"
+                onClick={this.fetchCat}
+              />
+            </div>
+            <div className="col-2">
+              <Button 
+                text="Get Dog"
+                type="button"
+                className="btn btn-light"
+                onClick={this.fetchDog}
+              />
+            </div>
+            <div className="col-4"></div>
+          </div>
+          
+          
         </div>
       </div>
     );
   }
 }
+
+const Button = ({ text, ...props }) => {
+  return <button {...props} >{text}</button>
+}
+  
 
 export default App;
